cf0b435876c780582b3cfcea12fcab84c218e8ae
Dorange <974391165@qq.com>
2020-07-02 17:26:28 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <g6-process/>
+  </div>
+</template>
+
+<script>
+import G6Process from "@/components/g6-process";
+export default {
+  name: "App",
+  components: { G6Process }
+};
+</script>
+
+<style scoped>
+#app {
+  width: 90vw;
+  height: 90vh;
+}
+</style>
+
